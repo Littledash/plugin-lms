b4b0052835b3ec7925375bca55425bb87c1c2809
Christopher Nowlan <hello@christophernowlan.com.au>
2025-07-31 17:14:45 +1000

    Remove roles from default student fields

--- src/fields/defaultStudentFields.ts
+++ src/fields/defaultStudentFields.ts
@@ -1,12 +1,10 @@
 import type { Field } from 'payload'
-import { rolesField } from './rolesField.js'
 import { enrolledCoursesField } from './enrolledCoursesField.js'
 import { completedCoursesField } from './completedCoursesField.js'
 import { certificatesField } from './certifcatesField.js'
 
 export const defaultStudentFields: () => Field[] = () => {
   return [
-    rolesField({}),
     enrolledCoursesField({}),
     completedCoursesField({}),
     certificatesField({}),
