b5c88f5ae27c0a90c584df308818535f7e967873
Christopher Nowlan <hello@christophernowlan.com.au>
2025-09-26 10:25:13 +1000

    Resolve enrolled course not being an array

--- src/utilities/courseCompletion.ts
+++ src/utilities/courseCompletion.ts
@@ -1,9 +1,8 @@
 import { CourseProgress, Progress } from '../providers/types.js'
 export const courseCompletion = (
-  export const courseCompletion = (
     progress: Progress,
     courseId: string,
-  ) => {
+  ): string => {
     const courseProgress = progress.find((p: CourseProgress) => 
       (typeof p.course === 'object' ? p.course.id : p.course) === courseId
     )
